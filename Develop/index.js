f0c2985bc8b3e6f550abdea0586418a4aea9d2b6
Angel Randall <drivenbysuccess21@gmail.com>
2020-03-26 14:19:07 -0500

    added questions and required packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,52 @@
-const questions = [
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
-];
+     inquirer
+    .prompt([
+      /* Pass your questions in here */
+          {
+              message:"What is your GitHub username?",
+              name:"Username"
+          },
+      {
+          message:"What license are you using?",
+          license:"License"
+      },
+      {
+          message:"What is the project title?",
+          title:"Title"
+      },
+      {
+          message:"Please provide a description of your project",
+          description:"Description"
+      },
+      {
+          message:"Please provide a table of contents",
+          contents:"Table of contents"
+      },
+      {
+          message:"What are the installation instructions?",
+          installation:"installation"
+      },
+      {
+          message:"Provide the usage",
+          usage:"Usage"
+      },
+      {
+          message:"Please list any contributors.",
+          contributors:"Contributers"
+      },
+      {
+          message:"Please provide information about the tests.",
+          tests:"Tests"
+      },
+      {
+          message:"Would you like to add any questions?",
+          questions:"questions"
+      },
+    
+]);
 
 function writeToFile(fileName, data) {
 }
